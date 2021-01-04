4375186b1774e19490fd9872a9942ab6bf14ad40
kosmical <david.mario.pieri@gmail.com>
2021-01-04 22:49:52 +0100

    refactored main function

--- javascript/main.js
+++ javascript/main.js
@@ -22,31 +22,50 @@ function createProductFigure(response) {
   const gridContainer = document.querySelector('.grid-container');
   // Loop through the data to create elements for all products
   for (let i in response) {
-    const figure = document.createElement('figure');
-    const imgSrc = response[i].imageUrl;
-    const img = document.createElement('img');
-    const figcaption = document.createElement('figcaption');
-    const a1 = document.createElement('a');
-    const a2 = document.createElement('a');
-    const p = document.createElement('p');
-
+    const {img, a1, a2, p, figcaption, figure} = createElements();
+    
     // Set element attributes and textcontent
-    img.setAttribute('src', imgSrc);
-    a1.setAttribute('href', 'product.html?id=' + response[i]._id);
-    a2.setAttribute('href', 'product.html?id=' + response[i]._id);
-    a1.textContent = response[i].name;
-    p.textContent = '$' + response[i].price / 100;
+    setAttributesAndText(response, i, img, a1, a2, p);
 
     // Add everything to their parent containers
-    a2.appendChild(img)
-    figcaption.appendChild(a1);
-    figcaption.appendChild(p);
-    figure.appendChild(a2);
-    figure.appendChild(figcaption);
-    gridContainer.appendChild(figure);
+    appendElements(a2, img, figcaption, a1, p, figure, gridContainer);
   }
 };
 
+
+function appendElements(a2, img, figcaption, a1, p, figure, gridContainer) {
+  a2.appendChild(img);
+  figcaption.appendChild(a1);
+  figcaption.appendChild(p);
+  figure.appendChild(a2);
+  figure.appendChild(figcaption);
+  gridContainer.appendChild(figure);
+}
+
+function setAttributesAndText(response, i, img, a1, a2, p) {
+  const imgSrc = response[i].imageUrl;
+  img.setAttribute('src', imgSrc);
+
+  let imgNr = 0;
+  for (let i in response) {
+    imgNr++;
+    img.setAttribute('alt', 'camera-' + imgNr[i]);
+  }
+  a1.setAttribute('href', 'product.html?id=' + response[i]._id);
+  a2.setAttribute('href', 'product.html?id=' + response[i]._id);
+  a1.textContent = response[i].name;
+  p.textContent = '$' + response[i].price / 100;
+}
+
+function createElements() {
+  const figure = document.createElement('figure');
+  const img = document.createElement('img');
+  const figcaption = document.createElement('figcaption');
+  const a1 = document.createElement('a');
+  const a2 = document.createElement('a');
+  const p = document.createElement('p');
+  return { img, a1, a2, p, figcaption, figure };
+}
 
 async function requestPromise() {
   try {
